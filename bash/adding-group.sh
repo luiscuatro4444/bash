01b4b6a5150b35e97ad4a6484d39ff34560cce82
luiscuatro4444 <luiscuatro4444@gmail.com>
2024-11-26 10:58:43 -0400

    Cleaning

--- bash/adding-group.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-user="usertomodify"
-group="grouptoadd"
-#Adding groups to a user
-
-usermod -aG $group $user
--- read-default.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# read-default: supply a default value if user presses the Enter key.
-
-read -e -p "What is your user name? " -i $USER
-echo "You answered: '$REPLY'"
